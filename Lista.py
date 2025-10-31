c52e10bcc766514b23f04974057c53a5783bb16c
1Ksantiago1 <ksantiagosanabria1123@gmail.com>
2025-10-31 07:36:12 -0500

    subida de archivos
    subo 3 archivos 1 es de adivinar el numero el otro es una calculadora y la ultima es una lista.

---
+++ Lista.py
@@ -0,0 +1,8 @@
+mi_lista = []
+#mi_lista.insert(7)
+#mi_lista.insert(15)
+#mi_lista.insert(23)
+mi_lista.append(10)
+mi_lista.append(4)
+mi_lista.append(1)
+print(mi_lista)
---
+++ adivinar el nuemro-2.py
@@ -0,0 +1,6 @@
+print("bienvenidos al juego de adivinar el numero")
+numero=float (input("escriba un numero del 1 al 10)"))
+if numero == 7:
+    print("ganaste")
+else:
+    print("fallaste")
